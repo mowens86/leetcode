55ab74777718d69c566f7f25eea10532f5f43705
Mike Owens <mocoding86@gmail.com>
2021-01-14 19:23:01 -0800

    solve shuffle-string algorithm

---
+++ shuffle-string.js
@@ -0,0 +1,18 @@
+// jshint esversion:6
+
+/**
+ * @param {string} s
+ * @param {number[]} indices
+ * @return {string}
+ */
+const restoreString = function(s, indices) {
+    const splitAnswer = [...indices];
+    const splitS = s.split("");
+    splitS.forEach((letter, num) => {
+        splitAnswer.splice(indices[num], 1, letter);
+    });
+    const answer = splitAnswer.join("");
+    return answer;
+};
+
+restoreString("codeleet", [4,5,6,7,0,2,1,3]);
